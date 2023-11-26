a6b4cbdbbffb3f1c3bd60ae5d78f37f54dabb9d3
Jéssica Paixão <jessmelo98@gmail.com>
2023-11-25 22:20:21 -0300

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,9 +5,9 @@ export default function NotFound() {
   return (
     <div>
     <div id="container" className="max-w-4xl justify-center rounded-2xl text-center mt-10">
-        <h2>404 - Not Found</h2>
-        <p>Could not find requested resource</p>
-        <Link href="/">Return Home</Link>
+        <h2 className="font-bold">404 - Not Found</h2>
+        <p>Could not find page! :(</p>
+        <Link href="/">Return to Home</Link>
       </div>
     </div>
   )
